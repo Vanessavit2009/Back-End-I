a30d1a4437541da2064631f32132376ddc6e1603
Vanessavit2009 <schulercardoso123@gmail.com>
2025-04-03 11:29:08 -0400

    atividade 01 03/03/2025

---
+++ Atividade01.js
@@ -0,0 +1,32 @@
+//01
+
+let nome = "Vanessa";
+let idade = "16";
+console.log("olá meu nome é ",nome,"e eu tenho ",idade,"anos");
+
+
+//02
+let string = "Meu nome"
+
+lower = string.toLowerCase()
+console.log(lower)
+
+upper = string.toUpperCase()
+console.log(upper)
+
+
+//03
+let num = "15"
+let int = 15
+
+let text = (typeof num)
+console.log(text)
+let numero = (typeof int)
+console.log(numero)
+
+// 04
+let peso = 53
+let altura = 1.70
+let imc = peso / (altura * altura)
+console.log(imc)
+
