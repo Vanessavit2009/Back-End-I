7d703448deec889442f39968dadbb36ad11d5671
Vanessavit2009 <schulercardoso123@gmail.com>
2025-04-10 11:40:10 -0400

    atividade 02

---
+++ atividade02.js
@@ -0,0 +1,37 @@
+//01
+
+let num1 = 50
+let num2 = 45
+
+let soma = (num1 + num2);
+console.log("Soma:" +soma);
+
+let sub = (num1 - num2);
+console.log("Subtração:" +sub);
+
+let mult = (num1 * num2);
+console.log("Multiplicação:" +mult);
+
+let div = (num1 / num2);
+console.log("Divisão:" +div)
+
+//02
+
+let idade1 = 20
+let idade2 = 21
+let resultado = idade1 == idade2 
+console.log(resultado);
+
+
+
+//03
+let idade = 20
+
+let mensagem = idade >= 18? "menor de idade": "menor de idade";
+console.log(mensagem);
+
+//04
+
+let numero = 20;
+let verificação = (numero % 2 === 0) ? "par" : "ímpar";
+console.log(`O numero ${numero} é ${verificação}`);
